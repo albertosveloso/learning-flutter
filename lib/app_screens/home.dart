2be07b96eb446527960569402ab6f4546826d2e9
Beto L <albertosveloso@gmail.com>
2019-01-28 16:06:30 -0200

    Custom fonts

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-  
     return Center(
-      child: Container(
-        alignment: Alignment.center,
-        color: Colors.deepPurple,
-        // width: 200.0,
-        // height: 100.0,
-        // margin: EdgeInsets.all(15.0),
-        // padding: EdgeInsets.all(5.0),
-        margin: EdgeInsets.only(right: 15.0, top: 20.0),
-        child: Text("Skate", textDirection: TextDirection.ltr)
-      )
-  );
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            // width: 200.0,
+            // height: 100.0,
+            // margin: EdgeInsets.all(15.0),
+            // padding: EdgeInsets.all(5.0),
+            margin: EdgeInsets.only(right: 15.0, top: 20.0),
+            child: Text("Flight",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none, 
+                    fontSize: 75.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white
+                    // fontStyle: FontStyle.italic
+                )
+            )
+        )
+    );
   }
 }
